ee8b55dec7db93a411a17a04004be355e60cefb8
Abdelrahman Abokamr <abdelrahmanabokamr@gmail.com>
2024-10-22 15:09:47 +0300

    Add files via upload

---
+++ age.c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// *** A PROGRAM TO KNOW YOUR AGE BY YEAR OF BIRTH ***
+void main(void){
+    int a1 ;
+    int a3 ;
+    int a2 =2024 ;
+printf("ENTER YOUR YEAR OF BIRTH :");
+scanf("%d",&a1);
+printf("your age is : %d\n",a2-a1);
+printf("ENTER BRO YEAR OF BIRTH :");
+scanf("%d",&a3);
+printf("your age is : %d\n",a2-a3);
+printf("dif is:%d\n",a3-a1);
+}
